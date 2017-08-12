57c3129e2d44613fb1dc61639fb7030cc09e5bfb
Mrbysco <Heeybysco@live.nl>
2017-08-13 01:23:32 +0200

    Fix sync

--- src/main/java/com/Mrbysco/JustARaftMod/config/RaftConfigGen.java
+++ src/main/java/com/Mrbysco/JustARaftMod/config/RaftConfigGen.java
@@ -3,6 +3,10 @@ package com.Mrbysco.JustARaftMod.config;
 import com.Mrbysco.JustARaftMod.Reference;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = Reference.MOD_ID, category = "")
 @Config.LangKey("jarm.config.title")
@@ -16,4 +20,14 @@ public class RaftConfigGen {
 		//public boolean CollisionBreak = true;
 	}
 	
+	@Mod.EventBusSubscriber(modid = Reference.MOD_ID)
+	private static class EventHandler {
+		
+		@SubscribeEvent
+		public static void onConfigChanged(final ConfigChangedEvent.OnConfigChangedEvent event) {
+			if (event.getModID().equals(Reference.MOD_ID)) {
+				ConfigManager.sync(Reference.MOD_ID, Config.Type.INSTANCE);
+			}
+		}
+	}
 }
