221fc2516087f4ef2e2a6d9b273a760d0e344adc
Mrbysco <Mrbysco@users.noreply.github.com>
2024-10-02 20:09:12 +0200

    Mark the RaftType constructor arguments NotNull

--- src/main/java/com/mrbysco/justaraftmod/entities/RaftType.java
+++ src/main/java/com/mrbysco/justaraftmod/entities/RaftType.java
@@ -4,6 +4,7 @@ import com.mrbysco.justaraftmod.Reference;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,7 +21,7 @@ public class RaftType {
 	private final Supplier<Item> raft;
 	private final ResourceLocation textureLocation;
 
-	public RaftType(Block planks, Supplier<Item> raft, String name, ResourceLocation textureLocation) {
+	public RaftType(@NotNull Block planks, @NotNull Supplier<Item> raft, @NotNull String name, @NotNull ResourceLocation textureLocation) {
 		this.id = nextId++;
 		this.name = name;
 		this.planks = planks;
@@ -28,7 +29,7 @@ public class RaftType {
 		this.textureLocation = textureLocation;
 	}
 
-	public RaftType(Block planks, Supplier<Item> raft, String name) {
+	public RaftType(@NotNull Block planks, @NotNull Supplier<Item> raft, @NotNull String name) {
 		this(planks, raft, name, Reference.modLoc("textures/entity/raft/" + name + "_raft.png"));
 	}
 
